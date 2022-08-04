9c8a557133a44633eddd744e0f56fb5f95311ec1
Alexis Mata WebDev <alexedmata@gmail.com>
2022-08-04 16:59:19 -0400

    #1 Empezando a construir el switch case

--- src/VillaPeruana.php
+++ src/VillaPeruana.php
@@ -70,4 +70,63 @@ class VillaPeruana
             }
         }
     }
+
+    public function tick_v2()
+    {
+        switch ($this->name) {
+            case 'Pisco Peruano':
+                 
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        if ($this->name != 'Pisco Peruano' and $this->name != 'Ticket VIP al concierto de Pick Floid') {
+            if ($this->quality > 0) {
+                if ($this->name != 'Tumi de Oro Moche') {
+                    $this->quality = $this->quality - 1;
+                }
+            }
+        } else {
+            if ($this->quality < 50) {
+                $this->quality = $this->quality + 1;
+
+                if ($this->name == 'Ticket VIP al concierto de Pick Floid') {
+                    if ($this->sellIn < 11) {
+                        if ($this->quality < 50) {
+                            $this->quality = $this->quality + 1;
+                        }
+                    }
+                    if ($this->sellIn < 6) {
+                        if ($this->quality < 50) {
+                            $this->quality = $this->quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($this->name != 'Tumi de Oro Moche') {
+            $this->sellIn = $this->sellIn - 1;
+        }
+
+        if ($this->sellIn < 0) {
+            if ($this->name != 'Pisco Peruano') {
+                if ($this->name != 'Ticket VIP al concierto de Pick Floid') {
+                    if ($this->quality > 0) {
+                        if ($this->name != 'Tumi de Oro Moche') {
+                            $this->quality = $this->quality - 1;
+                        }
+                    }
+                } else {
+                    $this->quality = $this->quality - $this->quality;
+                }
+            } else {
+                if ($this->quality < 50) {
+                    $this->quality = $this->quality + 1;
+                }
+            }
+        }
+    }
 }
